77c905dbf8f25e9287b446ce1c6b30bd677e3fe7
Denis Nikitin <d.nikitin@cian.ru>
2020-02-05 19:42:01 +0300

    cs

--- src/my_offers/services/offers/__init__.py
+++ src/my_offers/services/offers/__init__.py
@@ -1,2 +1,2 @@
-from my_offers.services.offers.update_offer_service import update_offer
 from my_offers.services.offers.get_offers_service import get_offers
+from my_offers.services.offers.update_offer_service import update_offer
