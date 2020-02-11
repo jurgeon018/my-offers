4ac79c8314563d8c7daaef4b5d11c7ff9253464e
o.korolev <o.korolev@cian.ru>
2020-02-11 13:12:02 +0300

    CD-72788: Апи поиска: активные объявления

---
+++ tests_api/settings/base.py
@@ -0,0 +1,18 @@
+import uuid
+from cian_automation.settings.base import *  # pylint: disable=wildcard-import,unused-wildcard-import
+
+
+X_PASS_KEY = '87C8573A-BD08-4A54-BF33-DBBD2AF78551'
+HEADERS = {
+    'X-PassKey': X_PASS_KEY,
+    'X-TestMode': '1',
+    'X-Real-Email': '',
+    'X-Authenticated': '1',
+    'X-EnableTwoFactor': 'false',
+}
+
+HEADERS_ON_TEST = {
+    'X-OperationId': lambda: str(uuid.uuid4()),
+}
+
+PATH_TO_SERVICES = 'tests_api.cian.services'
