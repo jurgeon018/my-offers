e88c4d01013f486489df6d69e6affcda2dc3075a
o.korolev <o.korolev@cian.ru>
2020-02-11 13:36:43 +0300

    CD-72788: Апи поиска: активные объявления

--- src/my_offers/services/offers/offer_view.py
+++ src/my_offers/services/offers/offer_view.py
@@ -2,7 +2,8 @@ from typing import List, Optional
 
 from my_offers.entities.get_offers import GetOffer
 from my_offers.entities.offer_view_model import Address, Newbuilding, OfferGeo, PriceInfo, Subagent, Underground
-from my_offers.repositories.monolith_cian_announcementapi.entities import ObjectModel
+from my_offers.repositories.monolith_cian_announcementapi.entities import ObjectModel, PublishTerms, BargainTerms, Geo
+from my_offers.repositories.monolith_cian_announcementapi.entities.object_model import Category
 from my_offers.repositories.monolith_cian_announcementapi.entities.publish_term import Services
 from my_offers.services.announcement.process_announcement_service import CATEGORY_OFFER_TYPE_DEAL_TYPE
 
@@ -14,9 +15,9 @@ def build_offer_view(raw_offer: ObjectModel) -> GetOffer:
     url_to_offer = None  # TODO: https://jira.cian.tech/browse/CD-73814
 
     geo = OfferGeo(
-        address=_get_address(raw_offer),
-        newbuilding=_get_newbuilding(raw_offer),
-        underground=_get_underground(raw_offer)
+        address=_get_address(raw_offer.geo),
+        newbuilding=_get_newbuilding(raw_offer.geo),
+        underground=_get_underground(raw_offer.geo)
     )
     subagent = (
         Subagent(id=raw_offer.published_user_id, name='')  # TODO: https://jira.cian.tech/browse/CD-73807
@@ -33,11 +34,11 @@ def build_offer_view(raw_offer: ObjectModel) -> GetOffer:
         url=url_to_offer,
         geo=geo,
         subagent=subagent,
-        price_info=_get_price(raw_offer),
-        features=_get_features(raw_offer),
-        publish_features=_get_publish_features(raw_offer),
-        vas=_get_vas(raw_offer),
-        is_from_package=_is_from_package(raw_offer),
+        price_info=_get_price(raw_offer.bargain_terms, raw_offer.category),
+        features=_get_features(raw_offer.bargain_terms, raw_offer.category),
+        publish_features=_get_publish_features(raw_offer.publish_terms),
+        vas=_get_vas(raw_offer.publish_terms),
+        is_from_package=_is_from_package(raw_offer.publish_terms),
         is_manual=source,
         is_publication_time_ends=_is_publication_time_ends(raw_offer),
     )
@@ -48,34 +49,34 @@ def _is_publication_time_ends(raw_offer: ObjectModel) -> bool:
     return False
 
 
-def _is_from_package(raw_offer: ObjectModel) -> bool:
-    if not raw_offer.publish_terms or not raw_offer.publish_terms.terms:
+def _is_from_package(publish_terms: PublishTerms) -> bool:
+    if not publish_terms or not publish_terms.terms:
         return False
 
     # publish_terms только для опубликованных
     return any(
         term.tariff_identificator.tariff_grid_type.is_service_package
-        for term in raw_offer.publish_terms.terms
+        for term in publish_terms.terms
         if term.tariff_identificator and term.tariff_identificator.tariff_grid_type
     )
 
 
-def _get_vas(raw_offer: ObjectModel) -> Optional[List[Services]]:
-    if not raw_offer.publish_terms or not raw_offer.publish_terms.terms:
+def _get_vas(publish_terms: PublishTerms) -> Optional[List[Services]]:
+    if not publish_terms or not publish_terms.terms:
         return None
 
     services: List[Services] = []
-    for t in raw_offer.publish_terms.terms:
+    for t in publish_terms.terms:
         if t.services:
             services.extend(t.services)
 
     return services
 
 
-def _get_price(raw_offer: ObjectModel) -> PriceInfo:
-    deal_type = _get_deal_type(raw_offer)
+def _get_price(bargain_terms: BargainTerms, category: Category) -> PriceInfo:
+    deal_type = _get_deal_type(category)
     is_rent = deal_type.is_rent
-    price = int(raw_offer.bargain_terms.price)
+    price = int(bargain_terms.price)
 
     # TODO: https://jira.cian.tech/browse/CD-73814
     if is_rent:
@@ -86,26 +87,24 @@ def _get_price(raw_offer: ObjectModel) -> PriceInfo:
     return PriceInfo(exact_price=exact_price)
 
 
-def _get_publish_features(raw_offer: ObjectModel) -> Optional[List[str]]:
-    if not raw_offer.publish_terms:
+def _get_publish_features(publish_terms: PublishTerms) -> Optional[List[str]]:
+    if not publish_terms:
         return None
 
     features = []
 
-    if raw_offer.publish_terms.autoprolong:
+    if publish_terms.autoprolong:
         features.append('автопродление')
 
-    if raw_offer.publish_terms.terms:
-        days = raw_offer.publish_terms.terms[0].days
+    if publish_terms.terms:
+        days = publish_terms.terms[0].days
         features.append(f'осталось {days} д.')  # TODO: https://jira.cian.tech/browse/CD-73814
 
     return features
 
 
-def _get_features(raw_offer: ObjectModel) -> List[str]:
-    bargain_terms = raw_offer.bargain_terms
-
-    deal_type = _get_deal_type(raw_offer)
+def _get_features(bargain_terms: BargainTerms, category: Category) -> List[str]:
+    deal_type = _get_deal_type(category)
     is_sale = deal_type.is_sale
     is_rent = deal_type.is_rent
     features = []
@@ -130,14 +129,14 @@ def _get_features(raw_offer: ObjectModel) -> List[str]:
     return features
 
 
-def _get_underground(raw_offer: ObjectModel) -> Optional[Underground]:
-    if not raw_offer.geo or not raw_offer.geo.undergrounds or not raw_offer.geo.address:
+def _get_underground(geo: Geo) -> Optional[Underground]:
+    if not geo or not geo.undergrounds or not geo.address:
         return None
 
     # получаем основное метро
-    undergrounds = list(filter(lambda x: x.is_default, raw_offer.geo.undergrounds))
+    undergrounds = list(filter(lambda x: x.is_default, geo.undergrounds))
     # определяем местоположение
-    address = list(filter(lambda x: x.type.is_location, raw_offer.geo.address))
+    address = list(filter(lambda x: x.type.is_location, geo.address))
 
     if undergrounds and address:
         return Underground(
@@ -150,18 +149,18 @@ def _get_underground(raw_offer: ObjectModel) -> Optional[Underground]:
     return None
 
 
-def _get_newbuilding(raw_offer: ObjectModel) -> Optional[Newbuilding]:
-    if not raw_offer.geo or not raw_offer.geo.jk:
+def _get_newbuilding(geo: Geo) -> Optional[Newbuilding]:
+    if not geo or not geo.jk:
         return None
 
-    return Newbuilding(search_url='', name=raw_offer.geo.jk.name)
+    return Newbuilding(search_url='', name=geo.jk.name)
 
 
-def _get_address(raw_offer: ObjectModel) -> Optional[Address]:
-    if not raw_offer.geo or not raw_offer.geo.address:
+def _get_address(geo: Geo) -> Optional[Address]:
+    if not geo or not geo.address:
         return None
 
-    addresses = raw_offer.geo.address
+    addresses = geo.address
 
     town = None
     district = None
@@ -184,6 +183,6 @@ def _get_address(raw_offer: ObjectModel) -> Optional[Address]:
     return Address(search_url='', name=address_name)
 
 
-def _get_deal_type(raw_offer: ObjectModel):
-    _, deal_type = CATEGORY_OFFER_TYPE_DEAL_TYPE[raw_offer.category]
+def _get_deal_type(category: Category) -> Category:
+    _, deal_type = CATEGORY_OFFER_TYPE_DEAL_TYPE[category]
     return deal_type
