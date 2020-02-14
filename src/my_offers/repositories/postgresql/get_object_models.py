b7d8d3efd84fb0c9a1ab5f9b998dd1cf36114e63
Denis Nikitin <d.nikitin@cian.ru>
2020-02-14 16:58:46 +0300

    repo refactor

--- src/my_offers/repositories/postgresql/get_object_models.py
+++ src/my_offers/repositories/postgresql/get_object_models.py
@@ -1,17 +1,12 @@
-import copy
-from datetime import datetime
 from typing import Any, Dict, List
 
 import asyncpgsa
-import pytz
 import sqlalchemy as sa
 from cian_json import json
 from sqlalchemy import and_, any_, cast, func, select
-from sqlalchemy.dialects.postgresql import insert
 
-from my_offers import entities, pg
+from my_offers import pg
 from my_offers.mappers.object_model import object_model_mapper
-from my_offers.mappers.offer_mapper import offer_mapper
 from my_offers.repositories.monolith_cian_announcementapi.entities import ObjectModel
 from my_offers.repositories.postgresql import tables
 
@@ -28,30 +23,6 @@ FILTERS_MAP = {
     'master_user_id': OFFER_TABLE.master_user_id,
     'sub_agent_ids': OFFER_TABLE.user_id,
 }
-
-
-async def save_offer(offer: entities.Offer) -> None:
-    values = offer_mapper.map_to(offer)
-
-    now = datetime.now(tz=pytz.UTC)
-    values['updated_at'] = now
-
-    update = copy.deepcopy(values)
-    del update['offer_id']
-
-    values['created_at'] = now
-
-    query, params = asyncpgsa.compile_query(
-        insert(tables.offers)
-        .values([values])
-        .on_conflict_do_update(
-            index_elements=[tables.offers.c.offer_id],
-            where=tables.offers.c.row_version < offer.row_version,
-            set_=update
-        )
-    )
-
-    await pg.get().execute(query, *params)
 
 
 async def get_object_models(
