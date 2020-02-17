c977a0c4b973a0df202c4507edef27443b59ad04
o.korolev <o.korolev@cian.ru>
2020-02-17 11:00:38 +0300

    CD-73814: Апи поиска: стабилизация offer_view

--- src/my_offers/helpers/numbers.py
+++ src/my_offers/helpers/numbers.py
@@ -10,9 +10,9 @@ def get_pretty_number(*, number: int, template: str = '%.0f') -> str:
     number_str = str(template % number)
 
     if '.' in number_str:
-        integral, fractional = number_str.split('.', 1)
+        integral = number_str.split('.')
     else:
-        integral, fractional = number_str, ''
+        integral = number_str
 
     result = []
     for _ in range(len(integral) // 3 + 1):
@@ -20,7 +20,5 @@ def get_pretty_number(*, number: int, template: str = '%.0f') -> str:
         integral = integral[:-3]
 
     integral = ' '.join(reversed(list(filter(None, result))))
-    if fractional.strip('0'):
-        return f'{integral},{fractional}'
 
     return integral
