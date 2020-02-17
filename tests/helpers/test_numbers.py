e9f0ab7b1abfa2643a66cc8d2406c004aeb84129
o.korolev <o.korolev@cian.ru>
2020-02-17 09:46:05 +0300

    CD-73814: Апи поиска: стабилизация offer_view

--- tests/helpers/test_numbers.py
+++ tests/helpers/test_numbers.py
@@ -10,7 +10,7 @@ from my_offers.helpers.numbers import get_pretty_number
     (10000, '10 000'),
     (100000, '100 000'),
     (1000000, '1 000 000'),
-    (1.5, '2'),
+    (1000.5, '1 000'),
 ])
 def test_get_pretty_number(number, expected):
     # act
