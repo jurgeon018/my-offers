d8af8e1e134e4c8dac44d58f4c9bb16ed685a35f
Denis Nikitin <d.nikitin@cian.ru>
2020-02-26 12:55:50 +0300

    CD-73196: offer action

--- src/my_offers/services/actions/_delete_offer.py
+++ src/my_offers/services/actions/_delete_offer.py
@@ -1,87 +1,44 @@
-import logging
-
-from cian_http.exceptions import ApiClientException
-from cian_web.exceptions import BrokenRulesException, Error
-
 from my_offers import entities, enums
-from my_offers.enums.offer_action_status import OfferActionStatus
 from my_offers.helpers.user_ids import get_realty_id_by_cian_id
 from my_offers.repositories.monolith_cian_announcementapi.entities import ObjectModel
 from my_offers.repositories.monolith_cian_realty import api_announcement_set_deleted
 from my_offers.repositories.monolith_cian_realty.entities import AnnouncementChangeStatus
 from my_offers.repositories.monolith_cian_realty.entities.announcement_change_status import AnnouncementType
-from my_offers.repositories.postgresql.object_model import get_object_model
+from my_offers.services.actions._action import OfferAction
 from my_offers.services.announcement import category
-from my_offers.services.get_master_user_id import get_master_user_id
-
-
-logger = logging.getLogger(__name__)
 
 
 class AnnouncementTypeError(Exception):
     pass
 
 
-async def delete_offer(request: entities.OfferActionRequest, realty_user_id: int) -> entities.OfferActionResponse:
-    offer_id = request.offer_id
-    object_model = await get_object_model({
-        'offer_id': offer_id,
-        'master_user_id': await get_master_user_id(realty_user_id),
-    })
-    if not object_model:
-        raise BrokenRulesException([
-            Error(
-                message='Объявление не найдено',
-                code='not_found',
-                key='offer_id'
-            )
-        ])
-
-    await _check_rights(user_id=realty_user_id, object_model=object_model)
-
-    offer_type, deal_type = category.get_types(object_model.category)
-    try:
+class DeleteOfferAction(OfferAction):
+    async def _run_action(self, object_model: ObjectModel) -> None:
+        offer_type, deal_type = category.get_types(object_model.category)
         await api_announcement_set_deleted(
             AnnouncementChangeStatus(
-                realty_object_id=offer_id,
-                announcement_type=_get_type_for_asp(offer_type=offer_type, deal_type=deal_type),
+                realty_object_id=object_model.id,
+                announcement_type=self._get_type_for_asp(offer_type=offer_type, deal_type=deal_type),
                 cian_announcement_id=object_model.cian_id,
-                cian_user_id=get_realty_id_by_cian_id(realty_user_id),
+                cian_user_id=get_realty_id_by_cian_id(self.user_id),
             )
         )
-    except ApiClientException:
-        logger.exception('Delete offer %s error', offer_id)
-        raise BrokenRulesException([
-            Error(
-                message='Ошибка при удаленни объявления',
-                code='operation_error',
-                key='offer_id'
-            )
-        ])
 
-    return entities.OfferActionResponse(status=OfferActionStatus.ok)
+    @staticmethod
+    def _get_type_for_asp(*, offer_type: enums.OfferType, deal_type: enums.DealType) -> AnnouncementType:
+        if offer_type.is_flat:
+            return AnnouncementType.flat if deal_type.is_rent else AnnouncementType.flat2
 
+        if offer_type.is_suburban:
+            return AnnouncementType.suburbian
 
-async def _check_rights(*, user_id: int, object_model: ObjectModel) -> None:
-    # todo: https://jira.cian.tech/browse/CD-74186
-    if user_id != object_model.user_id:
-        raise BrokenRulesException([
-            Error(
-                message='Не хватает прав для выполнения данной операции',
-                code='not_enough_rights',
-                key='offer_id'
-            )
-        ])
+        if offer_type.is_commercial:
+            return AnnouncementType.office
+
+        raise AnnouncementTypeError('Сan not determine AnnouncementType')
 
 
-def _get_type_for_asp(*, offer_type: enums.OfferType, deal_type: enums.DealType) -> AnnouncementType:
-    if offer_type.is_flat:
-        return AnnouncementType.flat if deal_type.is_rent else AnnouncementType.flat2
+async def delete_offer(request: entities.OfferActionRequest, realty_user_id: int) -> entities.OfferActionResponse:
+    action = DeleteOfferAction(offer_id=request.offer_id, user_id=realty_user_id)
 
-    if offer_type.is_suburban:
-        return AnnouncementType.suburbian
-
-    if offer_type.is_commercial:
-        return AnnouncementType.office
-
-    raise AnnouncementTypeError('Сan not determine AnnouncementType')
+    return await action.execute()
