640d4b3eeeb5f89ee3e792cc30029baefa9dc002
Denis Nikitin <d.nikitin@cian.ru>
2020-04-17 12:29:30 +0300

    CD-79081: fix filters

--- src/my_offers/repositories/postgresql/offer_conditions.py
+++ src/my_offers/repositories/postgresql/offer_conditions.py
@@ -1,7 +1,7 @@
 from typing import Any, Dict, List
 
 import sqlalchemy as sa
-from sqlalchemy import any_, cast
+from sqlalchemy import any_, cast, func
 
 from my_offers.repositories.postgresql import tables
 
@@ -23,6 +23,19 @@ FILTERS_MAP = {
 
 
 def prepare_conditions(filters: Dict[str, Any]) -> List:
+    conditions = _prepare_basic_conditions(filters)
+
+    if services := filters.get('services'):
+        conditions.append(OFFER_TABLE.services.overlap(services))
+    if search_text := filters.get('search_text'):
+        tsquery = func.plainto_tsquery('russian', search_text)
+        tsvector = func.to_tsvector('russian', OFFER_TABLE.search_text)
+        conditions.append(tsvector.op('@@')(tsquery))
+
+    return conditions
+
+
+def _prepare_basic_conditions(filters: Dict[str, Any]) -> List:
     conditions = []
     for key, value in filters.items():
         if key not in FILTERS_MAP:
