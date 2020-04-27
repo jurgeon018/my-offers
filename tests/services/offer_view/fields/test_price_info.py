0494090a5850f13f2156ae7384857e622c52b339
Denis Nikitin <d.nikitin@cian.ru>
2020-04-27 12:07:57 +0300

    tests

--- tests/services/offer_view/fields/test_price_info.py
+++ tests/services/offer_view/fields/test_price_info.py
@@ -5,8 +5,11 @@ from my_offers.entities.offer_view_model import PriceInfo
 from my_offers.repositories.monolith_cian_announcementapi.entities import BargainTerms
 from my_offers.repositories.monolith_cian_announcementapi.entities.bargain_terms import (
     Currency,
+    LeaseTermType,
+    LeaseType,
     PaymentPeriod,
     PriceType,
+    VatType,
 )
 from my_offers.repositories.monolith_cian_announcementapi.entities.object_model import Category
 from my_offers.services.offer_view.fields.price_info import get_price_info
@@ -35,6 +38,17 @@ from my_offers.services.offer_view.fields.price_info import get_price_info
             enums.OfferType.commercial,
             enums.DealType.sale,
             PriceInfo(exact=None, range=['от 1 000', 'до 10 000 ₽']),
+        ),
+        (
+            BargainTerms(price=10000.0, currency=Currency.rur, price_type=PriceType.all),
+            Category.daily_flat_rent,
+            True,
+            10,
+            100,
+            100,
+            enums.OfferType.flat,
+            enums.DealType.rent,
+            PriceInfo(exact='10 000 ₽/сут.', range=None),
         ),
         (
             BargainTerms(
@@ -83,6 +97,29 @@ from my_offers.services.offer_view.fields.price_info import get_price_info
             enums.OfferType.commercial,
             enums.DealType.rent,
             PriceInfo(exact=None, range=None),
+        ),
+        (
+            BargainTerms(
+                price=18000.0,
+                vat_type=VatType.included,
+                currency=Currency.rur,
+                price_type=PriceType.square_meter,
+                payment_period=PaymentPeriod.annual,
+                vat_price=3000.0,
+                client_fee=0,
+                lease_type=LeaseType.direct,
+                vat_included=True,
+                prepay_months=1,
+                lease_term_type=LeaseTermType.long_term,
+            ),
+            Category.office_rent,
+            False,
+            10,
+            100,
+            100,
+            enums.OfferType.commercial,
+            enums.DealType.rent,
+            PriceInfo(exact='150 000 ₽/мес.', range=None),
         ),
     ),
 
