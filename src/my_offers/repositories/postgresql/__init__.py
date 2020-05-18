e028b35ebf25af695885198d555982e7b5884f1a
Denis Nikitin <d.nikitin@cian.ru>
2020-05-18 19:07:03 +0300

    CD-81073: update offer master user id

--- src/my_offers/repositories/postgresql/__init__.py
+++ src/my_offers/repositories/postgresql/__init__.py
@@ -1,11 +1,7 @@
 from my_offers.repositories.postgresql.agents import save_agent
-from my_offers.repositories.postgresql.billing import (
-    get_offer_contract,
-    save_offer_contract,
-    set_offer_contract_is_deleted_status,
-)
+from my_offers.repositories.postgresql.billing import save_offer_contract, set_offer_contract_is_deleted_status
 from my_offers.repositories.postgresql.moderation import get_offers_offence, save_offer_offence
 from my_offers.repositories.postgresql.object_model import get_object_models
-from my_offers.repositories.postgresql.offer import get_offers_creation_date, save_offer
+from my_offers.repositories.postgresql.offer import get_offers_creation_date, save_offer, update_offer_master_user_id
 from my_offers.repositories.postgresql.offer_import_error import delete_offer_import_error
 from my_offers.repositories.postgresql.offers_duplicates import delete_offers_duplicates, update_offers_duplicates
