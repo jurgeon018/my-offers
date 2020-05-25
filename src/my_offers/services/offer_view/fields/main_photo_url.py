5a34f94b7752d479511fdc7af5bec33a55e72d72
k.snacheva <k.snacheva@cian.ru>
2020-05-25 17:11:14 +0300

    CD-81398 done

--- src/my_offers/services/offer_view/fields/main_photo_url.py
+++ src/my_offers/services/offer_view/fields/main_photo_url.py
@@ -3,5 +3,10 @@ from typing import List, Optional
 from my_offers.repositories.monolith_cian_announcementapi.entities import Photo
 
 
-def get_main_photo_url(photos: Optional[List[Photo]]) -> Optional[str]:
-    return photos[0].mini_url if photos else None
+def get_main_photo_url(
+        photos: Optional[List[Photo]],
+        better_quality: bool = False
+) -> Optional[str]:
+    if not photos:
+        return None
+    return photos[0].thumbnail_url if better_quality else photos[0].mini_url
