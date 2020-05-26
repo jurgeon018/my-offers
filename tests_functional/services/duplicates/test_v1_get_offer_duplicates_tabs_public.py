cd056f301bdbc082539f1d9431c726324daea240
k.snacheva <k.snacheva@cian.ru>
2020-05-26 13:25:56 +0300

    CD-81335 tests

---
+++ tests_functional/services/duplicates/test_v1_get_offer_duplicates_tabs_public.py
@@ -0,0 +1,73 @@
+import os
+
+from tests_functional.utils import load_data
+
+
+async def test_v2_get_offer_tabs_public__duplicates_found__200(http_client, pg, auction_mock):
+    # arrange
+    await pg.execute(load_data(os.path.dirname(__file__) + '/../../', 'offers.sql'))
+    await pg.execute('INSERT INTO offers_duplicates values(231655140, 231655140, \'2020-05-26\')')
+    await pg.execute('INSERT INTO offers_duplicates values(231659418, 231655140, \'2020-05-26\')')
+    await pg.execute('INSERT INTO offers_duplicates values(173975523, 231655140, \'2020-05-26\')')
+
+    # act
+    response = await http_client.request(
+        'POST',
+        '/public/v1/get-offer-duplicates-tabs/',
+        json={'offerId': 231655140},
+        headers={'X-Real-UserId': 47135244},
+    )
+
+    # assert
+    assert response.data == {
+        'tabs': [
+            {'title': 'Все', 'type': 'all', 'count': 2},
+            {'title': 'Дубли', 'type': 'duplicate', 'count': 2}
+        ]
+    }
+
+
+async def test_v2_get_offer_tabs_public__duplicates_not_found__200(http_client, pg):
+    # arrange
+    await pg.execute(load_data(os.path.dirname(__file__) + '/../../', 'offers.sql'))
+
+    # act
+    response = await http_client.request(
+        'POST',
+        '/public/v1/get-offer-duplicates-tabs/',
+        json={'offerId': 231655140},
+        headers={'X-Real-UserId': 47135244},
+    )
+
+    # assert
+    assert response.data == {'tabs': []}
+
+
+async def test_v2_get_offer_tab_public__not_validate_offer__200(http_client, pg):
+    # arrange
+    await pg.execute(load_data(os.path.dirname(__file__) + '/../../', 'offers.sql'))
+
+    # act
+    response = await http_client.request(
+        'POST',
+        '/public/v1/get-offer-duplicates-tabs/',
+        json={'offerId': 209194477},
+        headers={'X-Real-UserId': 29437831},
+    )
+
+    # assert
+    assert response.data == {'tabs': []}
+
+
+async def test_v2_get_offers_tabs_public__offer_not_found__400(http_client):
+    # act
+    response = await http_client.request(
+        'POST',
+        '/public/v1/get-offer-duplicates-tabs/',
+        json={'offerId': 165491301},
+        headers={'X-Real-UserId': 1111},
+        expected_status=400,
+    )
+
+    # assert
+    assert response.data['errors'][0]['code'] == 'notFound'
