958c969d81e6a050e456269f7c6112787141e805
k.snacheva <k.snacheva@cian.ru>
2020-06-03 16:02:32 +0300

    CD-82071 review fix is_house

--- src/my_offers/services/valuation/fields/house_id.py
+++ src/my_offers/services/valuation/fields/house_id.py
@@ -3,14 +3,13 @@ from typing import List, Optional
 from cian_web.exceptions import BrokenRulesException, Error
 
 from my_offers.repositories.monolith_cian_announcementapi.entities import AddressInfo
-from my_offers.repositories.monolith_cian_announcementapi.entities.address_info import Type
 
 
 def get_house_id(
         address: List[AddressInfo]
 ) -> Optional[int]:
     for detail in address:
-        if detail.type == Type.house:
+        if detail.type.is_house:
             return detail.id
     raise BrokenRulesException([
         Error(
