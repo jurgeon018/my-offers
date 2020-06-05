1f5aa5b9167f1b9565b12d4f76a2de08ed4b9712
o.korolev <o.korolev@cian.ru>
2020-06-05 16:19:03 +0300

    CD-81374: Прокси к апи восстановить все из неактивных

--- src/my_offers/services/actions/_mass_restore.py
+++ src/my_offers/services/actions/_mass_restore.py
@@ -1,17 +1,22 @@
 import asyncio
-from typing import List, Optional
+from typing import List, Tuple
 
 from cian_web.exceptions import BrokenRulesException, Error
 from simple_settings import settings
 
-from my_offers import entities
+from my_offers import entities, helpers
+from my_offers.enums import GetOffersSortType
 from my_offers.repositories import monolith_cian_announcementapi, postgresql
 from my_offers.repositories.monolith_cian_announcementapi.entities import (
     AnnouncementProgressDto,
     AnnouncementsActionsV1GetJobStatus,
     GetJobStatusResponse,
+    ObjectModel,
     RestoreRequest,
     RestoreResponse,
+)
+from my_offers.repositories.monolith_cian_announcementapi.entities.announcement_progress_dto import (
+    State as OfferMassRestoreState,
 )
 from my_offers.repositories.monolith_cian_announcementapi.entities.get_job_status_response import State as JobState
 from my_offers.services.offers import get_filters
@@ -31,32 +36,35 @@ async def mass_offers_restore(
 ) -> entities.OffersMassRestoreResponse:
     """ Массово восстановить все объявление для пользователя """
     # TODO: перенести в Actions: https://jira.cian.tech/browse/CD-82000
-    # TODO: check rights
-    offers_statuses = []
-    offers_ids = request.offers_ids
 
-    if not offers_ids and request.action_type.is_select:
+    offers_statuses = []
+
+    if not request.offers_ids and request.action_type.is_select:
         raise BrokenRulesException(errors=[Error(
             key='offers_ids',
             code='offers_ids_is_empty',
             message=f'offers_ids is empty with type `{request.action_type.name}`'
         )])
 
-    if request.action_type.is_all:
-        filters = await get_filters(
-            user_id=realty_user_id,
-            filters=request.filters
-        )
-        offers_ids = await postgresql.get_offers_ids_by_tab(
-            filters=filters
-        )
+    filters = await get_filters(
+        user_id=realty_user_id,
+        filters=request.filters
+    )
+    objects_models, total = await postgresql.get_object_models(
+        filters=filters,
+        limit=300,  # TODO: settinga
+        offset=0,
+        sort_type=GetOffersSortType.by_default
+    )
+    offers_ids, offers_errors = _filter_offers(objects_models=objects_models)
+    offers_statuses.extend(offers_errors)
 
     if not offers_ids:
         return entities.OffersMassRestoreResponse(offers=[])
 
-    offers_final_statuses = await _get_job_result(offers_ids=offers_ids, realty_user_id=realty_user_id)
+    offers_final_statuses = await _run_job(offers_ids=offers_ids, realty_user_id=realty_user_id)
     if offers_final_statuses:
-        offers_statuses = [
+        offers_statuses += [
             entities.OfferMassRestoreStatus(
                 offer_id=offer.id,
                 status=offer.state,
@@ -64,10 +72,10 @@ async def mass_offers_restore(
             ) for offer in offers_final_statuses
         ]
 
-        return entities.OffersMassRestoreResponse(offers=offers_statuses)
+    return entities.OffersMassRestoreResponse(offers=offers_statuses)
 
 
-async def _get_job_result(*, offers_ids: List[int], realty_user_id: int) -> Optional[List[AnnouncementProgressDto]]:
+async def _run_job(*, offers_ids: List[int], realty_user_id: int) -> List[AnnouncementProgressDto]:
     session: RestoreResponse = await monolith_cian_announcementapi.announcements_actions_v1_restore(
         RestoreRequest(
             announcement_ids=offers_ids,
@@ -87,4 +95,24 @@ async def _get_job_result(*, offers_ids: List[int], realty_user_id: int) -> Opti
         if job_status.state in END_STATUSES:
             break
 
-    return job_status.announcements_progress
+    return job_status.announcements_progress or []
+
+
+def _filter_offers(objects_models: List[ObjectModel]) -> Tuple[List[int], List[entities.OfferMassRestoreStatus]]:
+    offers_ids = []
+    offers_errors = []
+
+    for o in objects_models:
+        print(2313231323131, o)
+        if helpers.is_manual(o.source):
+            offers_ids.append(o.id)
+        else:
+            offers_errors.append(
+                entities.OfferMassRestoreStatus(
+                    offer_id=o.id,
+                    status=OfferMassRestoreState.error,
+                    message='Нельзя автоматически восстановить XML'
+                )
+            )
+
+    return offers_ids, offers_errors
