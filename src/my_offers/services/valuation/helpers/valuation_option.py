ed0f713736adaeffdb96b8078da46be9c3acd078
k.snacheva <k.snacheva@cian.ru>
2020-06-09 21:31:27 +0300

    CD-82139 done

---
+++ src/my_offers/services/valuation/helpers/valuation_option.py
@@ -0,0 +1,50 @@
+from typing import List
+
+from my_offers.entities import ValuationOption
+from my_offers.enums import DealType
+from my_offers.repositories.monolith_cian_announcementapi.entities import PublishTerms
+from my_offers.repositories.price_estimator.entities import GetEstimationForRealtorsResponse
+from my_offers.services.valuation.fields.liquidity import get_liquidity_diapason
+from my_offers.services.valuation.helpers.pretty_price import get_pretty_market_price, get_pretty_price_diapason
+
+
+def get_valuation_options(
+        deal_type: DealType,
+        publish_terms: PublishTerms,
+        valuation_response: GetEstimationForRealtorsResponse
+) -> List[ValuationOption]:
+    market_price_str = get_pretty_market_price(valuation_response.prices.price)
+    price_diapason = get_pretty_price_diapason(
+        price_min=valuation_response.prices.price_min,
+        price_max=valuation_response.prices.price_max,
+    )
+    if deal_type.is_rent:
+        return [
+            ValuationOption(
+                value=f'{market_price_str}\xa0₽/мес',
+                description='Рыночная\xa0ставка'
+            ),
+            ValuationOption(
+                value=f'{price_diapason}\xa0₽/мес',
+                description='Диапазон\xa0ставки'
+            ),
+        ]
+
+    liquidity_diapason = get_liquidity_diapason(
+        publish_terms=publish_terms,
+        periods_range=valuation_response.liquidity_periods,
+    )
+    return [
+        ValuationOption(
+            value=f'{market_price_str}\xa0₽',
+            description='Рыночная\xa0цена\xa0этой\xa0квартиры'
+        ),
+        ValuationOption(
+            value=f'{price_diapason}\xa0₽',
+            description='Диапазон\xa0цены'
+        ),
+        ValuationOption(
+            value=f'{liquidity_diapason}\xa0дней',
+            description='Прогнозируемый срок продажи при текущей цене квартиры и продвижении'
+        )
+    ]
--- tests/services/valuation/fields/test_info_relative_market.py
+++ tests/services/valuation/fields/test_info_relative_market.py
@@ -1,37 +1,59 @@
 import pytest
 
 from my_offers.entities.valuation import InfoRelativeMarket
-from my_offers.enums import PriceEstimate
+from my_offers.enums import DealType, PriceEstimate
 from my_offers.services.valuation.fields.info_relative_market import get_info_relative_market
 
 
 @pytest.mark.parametrize(
-    ('market_price', 'real_price', 'expected'),
+    ('deal_type', 'market_price', 'real_price', 'expected'),
     (
-        (1_000_000, 2_000_000, InfoRelativeMarket(
+        (DealType.rent, 1_000_000, 2_000_000, InfoRelativeMarket(
+            price_estimate=PriceEstimate.more_market,
+            title='Ваша ставка выше рыночной',
+            text='У объявлений с арендной ставкой выше рынка в среднем на 45% меньше просмотров',
+            hint='Согласно статистике просмотров объявлений похожих квартир за последние 12 месяцев'
+        )),
+        (DealType.rent, 1_000_000, 500_000, InfoRelativeMarket(
+            price_estimate=PriceEstimate.less_market,
+            title='Ваша ставка ниже рыночной',
+            text='Квартиры с арендной ставкой ниже рынка и продвижением в среднем сдаются менее 14 дней',
+            hint='По данным Циан.Аналитика за предыдущий год'
+        )),
+        (DealType.rent, 1_000_000, 1_000_003, InfoRelativeMarket(
+            price_estimate=PriceEstimate.in_market,
+            title='Ваша ставка рыночная',
+            text='Квартиры с рыночной арендной ставкой и продвижением в среднем сдаются в течение 14 дней',
+            hint='По данным Циан.Аналитика за предыдущий год'
+        )),
+        (DealType.sale, 1_000_000, 2_000_000, InfoRelativeMarket(
             price_estimate=PriceEstimate.more_market,
             title='Ваша цена выше рыночной',
-            text='У объявлений с ценой выше рынка в среднем на 35% меньше просмотров.',
-            hint='Согласно статистике просмотров объявлений похожих квартир за последние 12 дней.'
+            text='У объявлений с ценой выше рынка в среднем на 35% меньше просмотров',
+            hint='Согласно статистике просмотров объявлений похожих квартир за последние 12 месяцев'
         )),
-        (1_000_000, 500_000, InfoRelativeMarket(
+        (DealType.sale, 1_000_000, 500_000, InfoRelativeMarket(
             price_estimate=PriceEstimate.less_market,
             title='Ваша цена ниже рыночной',
-            text='Квартиры с ценой ниже рынка и продвижением в среднем продаются в течение 30 дней.',
-            hint='Согласно данным Циан.Аналитики за предыдущий год.'
+            text='Квартиры с ценой ниже рынка и продвижением в среднем продаются в течение 30 дней',
+            hint='По данным Циан.Аналитика за предыдущий год'
         )),
-        (1_000_000, 1_000_000, InfoRelativeMarket(
+        (DealType.sale, 1_000_000, 1_000_003, InfoRelativeMarket(
             price_estimate=PriceEstimate.in_market,
             title='Ваша цена рыночная',
-            text='Квартиры с рыночной ценой и продвижением в среднем продаются в течение 45 дней.',
-            hint='Согласно данным Циан.Аналитики за предыдущий год.'
+            text='Квартиры с рыночной ценой и продвижением в среднем продаются в течение 45 дней',
+            hint='По данным Циан.Аналитика за предыдущий год'
         )),
 
     )
 )
-def test_get_info_relative_market(mocker, market_price, real_price, expected):
+def test_get_info_relative_market(mocker, deal_type, market_price, real_price, expected):
     # arrange & act
-    result = get_info_relative_market(market_price=market_price, real_price=real_price)
+    result = get_info_relative_market(
+        deal_type=deal_type,
+        market_price=market_price,
+        real_price=real_price,
+    )
 
     # assert
     assert result == expected
