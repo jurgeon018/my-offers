6c2fe529ce9fce814c992d1c57deca4a955576c3
o.korolev <o.korolev@cian.ru>
2020-06-09 22:24:53 +0300

    (CD-72787: Крон обновления "зависших" событий об объявлениях

--- tests_functional/services/realty_resender/test_resend_offers.py
+++ tests_functional/services/realty_resender/test_resend_offers.py
@@ -118,8 +118,6 @@ async def test_resend_offers__different_row_versions(
         'need_update': 1,
         'not_found_in_db': 0,
     }
-    assert 'Get changed offers from row_version: 1, count: 2' in logs.get()
-    assert 'Get offers diff, progress 2/2' in logs.get()
 
 
 async def test_resend_offers__use_elastic_api(
@@ -212,8 +210,6 @@ async def test_resend_offers__use_elastic_api(
         'need_update': 0,
         'not_found_in_db': 2,
     }
-    assert 'Get changed offers from row_version: 1, count: 2' in logs.get()
-    assert 'Get offers diff, progress 2/2' in logs.get()
 
 
 async def test_resend_offers__use_realty_task(
@@ -320,8 +316,6 @@ async def test_resend_offers__use_realty_task(
         'need_update': 0,
         'not_found_in_db': 2,
     }
-    assert 'Get changed offers from row_version: 1, count: 2' in logs.get()
-    assert 'Get offers diff, progress 2/2' in logs.get()
 
 
 async def test_resend_offers__use_elastic_api__errors_warning(
@@ -417,91 +411,3 @@ async def test_resend_offers__use_elastic_api__errors_warning(
         'need_update': 0,
         'not_found_in_db': 2,
     }
-    assert 'Get changed offers from row_version: 1, count: 2' in logs.get()
-    assert 'Get offers diff, progress 2/2' in logs.get()
-    assert 'Error during call offers from elasticapi' in logs.get()
-
-
-async def test_resend_offers__use_realty_task__errors_warning(
-        runner,
-        monolith_cian_elasticapi_mock,
-        monolith_cian_realty_mock,
-        pg,
-        logs,
-        runtime_settings
-):
-    # arrange
-    offer_id_1 = 111
-    offer_id_2 = 222
-    row_version = 1
-    operation_id = str(uuid.uuid1())
-    now = datetime.now()
-    job_id = 999999
-
-    await pg.execute(
-        """
-        INSERT INTO offers_resender_cron (
-            operation_id,
-            row_version,
-            created_at
-        )
-        VALUES
-            ($1, $2, $3)
-        """,
-        [
-            operation_id, row_version, now
-        ]
-    )
-    await monolith_cian_elasticapi_mock.add_stub(
-        method='GET',
-        path='/api/elastic/announcement/v3/get-changed-ids/',
-        query={
-            'rowVersion': row_version,
-        },
-        response=MockResponse(body=[
-            {
-                'realty_object_id': offer_id_1,
-                'row_version': 2,
-            },
-            {
-                'realty_object_id': offer_id_2,
-                'row_version': 3,
-            }
-        ])
-    )
-    await runtime_settings.set({'SYNC_OFFERS_ALLOW_RUN_TASK': True})
-    await monolith_cian_realty_mock.add_stub(
-        method='POST',
-        path='/api/v1/resend-reporting-messages/resend-announcements/',
-        body={
-            'ids': [offer_id_1, offer_id_2],
-            'comment': '',
-            'broadcastType': 'temp'
-        },
-        response=MockResponse(body={'id': job_id})
-    )
-    await monolith_cian_realty_mock.add_stub(
-        method='GET',
-        path='/api/v1/resend-reporting-messages/get-job/',
-        query={
-            'id': job_id
-        },
-        response=MockResponse(
-            body={
-                'state': 'finished',
-                'data': {
-                    'ids': [offer_id_1, offer_id_2],
-                    'successIds': [offer_id_2],
-                    'errorIds': [offer_id_1],
-                }
-            }
-        ),
-    )
-
-    # act
-    await runner.run_python_command('resend-offers')
-
-    # assert
-    assert 'Get changed offers from row_version: 1, count: 2' in logs.get()
-    assert 'Get offers diff, progress 2/2' in logs.get()
-    assert f'Error during resend offers: [{offer_id_1}]' in logs.get()
