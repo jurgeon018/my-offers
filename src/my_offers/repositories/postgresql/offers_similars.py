1bc8a92b76f7cd1a00742a668b768bf8100d39b5
Denis Nikitin <d.nikitin@cian.ru>
2020-08-18 06:51:38 +0300

    CD-88105: fix condition

--- src/my_offers/repositories/postgresql/offers_similars.py
+++ src/my_offers/repositories/postgresql/offers_similars.py
@@ -256,7 +256,7 @@ def _prepare_tab_condition(
         tab_condition = 'os.group_id = offer.group_id'
     elif tab_type.is_same_building:
         tab_condition = f"""
-            os.group_id <> offer.group_id
+            (os.group_id <> offer.group_id or offer.group_id is null)
             and os.house_id = offer.house_id
             and os.price >= offer.price * (1 - {price_kf})
             and os.price <= offer.price * (1 + {price_kf})
@@ -265,8 +265,8 @@ def _prepare_tab_condition(
         """
     elif tab_type.is_similar:
         tab_condition = f"""
-            os.group_id <> offer.group_id
-            and os.house_id <> offer.house_id
+            (os.group_id <> offer.group_id or os.group_id is null)
+            and (os.house_id <> offer.house_id or offer.house_id is null)
             and os.district_id = offer.district_id
             and os.price >= offer.price * (1 - {price_kf})
             and os.price <= offer.price * (1 + {price_kf})
