a26a584c74c9573eaa4e12872a32df4bb5ccad43
Denis Nikitin <d.nikitin@cian.ru>
2020-09-01 18:00:20 +0300

    CD-88941: tests

--- tests/services/duplicates/test_update_offers_duplicates.py
+++ tests/services/duplicates/test_update_offers_duplicates.py
@@ -1,8 +1,7 @@
 import pytest
 from cian_test_utils import future
 
-from my_offers.services.duplicates import update_offers_duplicates
-
+from my_offers.services.duplicates import update_offers_duplicates, update_offers_duplicate
 
 PATH = 'my_offers.services.duplicates._update_offer_duplicates.'
 
@@ -32,6 +31,21 @@ async def test_update_offers_duplicates__no_offers__return(mocker):
 
     # act
     await update_offers_duplicates([1])
+
+    # arrange
+    get_offers_row_version_mock.assert_called_once_with([1])
+
+
+@pytest.mark.gen_test
+async def test_update_offers_duplicate_no_offers__return(mocker):
+    # arrange
+    get_offers_row_version_mock = mocker.patch(
+        f'{PATH}get_offers_row_version',
+        return_value=future()
+    )
+
+    # act
+    await update_offers_duplicate(1)
 
     # arrange
     get_offers_row_version_mock.assert_called_once_with([1])
