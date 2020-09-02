e8e6736ad3e047b682c599a5b68c480d16205240
Denis Nikitin <d.nikitin@cian.ru>
2020-09-02 16:58:14 +0300

    tets

--- tests/repositories/postgresql/test_offers_duplicates.py
+++ tests/repositories/postgresql/test_offers_duplicates.py
@@ -1,10 +1,10 @@
-import pytest
+from cian_test_utils import future
 
 from my_offers import pg
 from my_offers.repositories.postgresql import delete_offers_duplicates
+from my_offers.repositories.postgresql.offers_duplicates import get_offer_duplicate_for_update
 
 
-@pytest.mark.gen_test
 async def test_delete_offers_duplicates(mocker):
     # arrange & act
     await delete_offers_duplicates([1, 2])
@@ -13,4 +13,19 @@ async def test_delete_offers_duplicates(mocker):
     pg.get().execute.assert_called_once_with(
         'DELETE FROM offers_duplicates WHERE offer_id = ANY($1::BIGINT[])',
         [1, 2],
+    )
+
+
+async def test_get_offer_duplicate_for_update(mocker):
+    # arrange
+    pg.get().fetchrow.return_value = future({'offer_id': 1})
+
+    # act
+    result = await get_offer_duplicate_for_update()
+
+    # assert
+    assert result == 1
+
+    pg.get().fetchrow.assert_called_once_with(
+        'select offer_id from offers_duplicates order by updated_at limit 1',
     )
