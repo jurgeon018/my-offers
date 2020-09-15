1c4ec6cbb0ad8fe14a05b23cc5d7a3b826a2712d
Александр Киверин <a.kiverin@cian.ru>
2020-09-15 11:34:57 +0300

    Добавлен функциональный тест кейса с колтрекингом в услугах

--- tests_functional/queue/test_save_announcement_contract_consumer.py
+++ tests_functional/queue/test_save_announcement_contract_consumer.py
@@ -25,13 +25,15 @@ async def test_process_save_announcement_correct_service_type(
       Проверка сохранения корректного типа услуги у приходящего контракта.
     """
     # arrange
+    now_isoformat = datetime.now(pytz.utc).isoformat()
+
     contract = {
         'id': 1,
         'user_id': 1,
         'actor_user_id': 1,
         'publisher_user_id': 1,
-        'start_date': datetime.now(pytz.utc).isoformat(),
-        'payed_till': datetime.now(pytz.utc).isoformat(),
+        'start_date': now_isoformat,
+        'payed_till': now_isoformat,
         'target_object_id': 1,
         'target_object_type': 'Announcement',
         'service_types': expected_service_types,
@@ -41,7 +43,7 @@ async def test_process_save_announcement_correct_service_type(
     message = {
         'service_contract_reporting_model': contract,
         'operation_id': '1',
-        'date': datetime.now(pytz.utc).isoformat(),
+        'date': now_isoformat,
     }
     # act
     await queue_service.wait_consumer('my-offers.save_announcement_contract')
@@ -64,6 +66,7 @@ async def test_process_save_announcement_empty_payed_till_returned_for_calltrack
       Проверка выдачи пустой даты payed_till в ответ на запрос объявлений, если у объявления есть только 1 контракт, в котором есть услуга calltracking.
     """
     # arrange
+    now_isoformat = datetime.now(pytz.utc).isoformat()
     await pg.execute_scripts(Path('tests_functional') / 'data' / 'offers.sql')
 
     contract = {
@@ -71,8 +74,8 @@ async def test_process_save_announcement_empty_payed_till_returned_for_calltrack
         'user_id': 1,
         'actor_user_id': 1,
         'publisher_user_id': 1,
-        'start_date': datetime.now(pytz.utc).isoformat(),
-        'payed_till': datetime.now(pytz.utc).isoformat(),
+        'start_date': now_isoformat,
+        'payed_till': now_isoformat,
         'target_object_id': 209194477,
         'target_object_type': 'Announcement',
         'service_types': ['calltracking'],
@@ -82,7 +85,7 @@ async def test_process_save_announcement_empty_payed_till_returned_for_calltrack
     message = {
         'service_contract_reporting_model': contract,
         'operation_id': '1',
-        'date': datetime.now(pytz.utc).isoformat(),
+        'date': now_isoformat,
     }
 
     # act
@@ -105,7 +108,7 @@ async def test_process_save_announcement_empty_payed_till_returned_for_calltrack
 
 
 @pytest.mark.asyncio
-async def test_process_save_announcement_correct_payed_till_returned(
+async def test_process_save_announcement_correct_payed_till_returned_without_calltracking(
     pg,
     queue_service,
     http
@@ -116,36 +119,39 @@ async def test_process_save_announcement_correct_payed_till_returned(
     # arrange
     await pg.execute_scripts(Path('tests_functional') / 'data' / 'offers.sql')
 
-    expected_date = datetime.now(pytz.utc)
+    contract1_date = datetime.now(pytz.utc)
     contract1 = {
         'id': 1,
         'user_id': 1,
         'actor_user_id': 1,
         'publisher_user_id': 1,
-        'start_date': datetime.now(pytz.utc).isoformat(),
-        'payed_till': expected_date.isoformat(),
+        'start_date': contract1_date.isoformat(),
+        'payed_till': contract1_date.isoformat(),
         'target_object_id': 209194477,
         'target_object_type': 'Announcement',
         'service_types': ['FreeObject'],
         'row_version': 1,
     }
-
-    contract2 = deepcopy(contract1)
-    contract2.update({
-        'service_types': ['FreeObject'],
-        'payed_till': (expected_date + timedelta(days=5)).isoformat(),
-        'id':2
-    }
-    )
-
     message1 = {
         'service_contract_reporting_model': contract1,
         'operation_id': '1',
-        'date': datetime.now(pytz.utc).isoformat(),
+        'date': contract1_date.isoformat(),
     }
+
+
+    contract2_date = contract1_date + timedelta(days=5)
+    contract2 = deepcopy(contract1)
+    contract2.update({
+        'service_types': ['FreeObject'],
+        'payed_till': contract2_date.isoformat(),
+        'id':2
+    }
+    )
     message2 = deepcopy(message1)
-    message2['service_contract_reporting_model'] = contract2
-    message2['operation_id'] = '2'
+    message2.update({
+        'service_contract_reporting_model': contract2,
+        'operation_id': '2'
+    })
 
     # act
     await queue_service.wait_consumer('my-offers.save_announcement_contract')
@@ -164,5 +170,72 @@ async def test_process_save_announcement_correct_payed_till_returned(
         json={
             'filters': {'status_tab': 'active'},
         })
-    print(await pg.fetchrow('SELECT count(*) FROM offers_billing_contracts'))
-    assert response.data['offers'][0]['pageSpecificInfo']['activeInfo']['payedTill'] == expected_date.isoformat()
+
+    assert response.data['offers'][0]['pageSpecificInfo']['activeInfo']['payedTill'] == contract2_date.isoformat()
+
+
+@pytest.mark.asyncio
+async def test_process_save_announcement_correct_payed_till_returned_with_calltracking(
+    pg,
+    queue_service,
+    http
+):
+    """
+      Проверка выдачи корректной даты payed_till если у объявления есть контракты c calltracking.
+    """
+    # arrange
+    await pg.execute_scripts(Path('tests_functional') / 'data' / 'offers.sql')
+
+    contract1_date = datetime.now(pytz.utc)
+    contract1 = {
+        'id': 1,
+        'user_id': 1,
+        'actor_user_id': 1,
+        'publisher_user_id': 1,
+        'start_date': contract1_date.isoformat(),
+        'payed_till': contract1_date.isoformat(),
+        'target_object_id': 209194477,
+        'target_object_type': 'Announcement',
+        'service_types': ['FreeObject'],
+        'row_version': 1,
+    }
+    message1 = {
+        'service_contract_reporting_model': contract1,
+        'operation_id': '1',
+        'date': contract1_date.isoformat(),
+    }
+
+
+    contract2_date = contract1_date + timedelta(days=5)
+    contract2 = deepcopy(contract1)
+    contract2.update({
+        'service_types': ['FreeObject', 'calltracking'],
+        'payed_till': contract2_date.isoformat(),
+        'id':2
+    }
+    )
+    message2 = deepcopy(message1)
+    message2.update({
+        'service_contract_reporting_model': contract2,
+        'operation_id': '2'
+    })
+
+    # act
+    await queue_service.wait_consumer('my-offers.save_announcement_contract')
+    await queue_service.publish('service-contract-reporting.v1.created', message1, exchange='billing')
+    await queue_service.publish('service-contract-reporting.v1.created', message2, exchange='billing')
+
+    await asyncio.sleep(2)
+
+    # assert
+    response = await http.request(
+        'POST',
+        '/public/v2/get-offers/',
+        headers={
+            'X-Real-UserId': 29437831
+        },
+        json={
+            'filters': {'status_tab': 'active'},
+        })
+
+    assert response.data['offers'][0]['pageSpecificInfo']['activeInfo']['payedTill'] == contract1_date.isoformat()
