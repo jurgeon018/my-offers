f4337d63bb78d2c84db46d63dd18be6db5f41957
Александр Киверин <a.kiverin@cian.ru>
2020-09-24 10:47:05 +0300

    Исправлен запрос обновления идентификатора плательщика

--- src/my_offers/repositories/postgresql/offer.py
+++ src/my_offers/repositories/postgresql/offer.py
@@ -305,28 +305,39 @@ async def get_offers_payed_by(offer_ids: List[int]) -> Dict[int, Optional[OfferP
 
 async def update_offers_master_user_id_and_payed_by(offer_ids: List[str]):
     query = """
-        with cte as (
+        with cte1 as (
         select
-            a.offer_id, b.master_agent_user_id, c.publisher_user_id
+            a.offer_id, b.master_agent_user_id
         from
             offers as a
         inner join
             agents_hierarchy as b on a.user_id = b.realty_user_id
-        inner join
-            offers_billing_contracts as c on a.offer_id = c.offer_id
-        where a.offer_id = ANY($1::BIGINT[])
+        where
+            a.offer_id = ANY($1::BIGINT[])
+        ), cte2 as (
+        select
+            c.offer_id, first_value (publisher_user_id)
+                        over (partition by offer_id order by row_version desc) as publisher_user_id
+        from
+            offers_billing_contracts as c
+        where
+            c.offer_id = ANY($1::BIGINT[])
         )
 
         update
             offers
         set
             old_master_user_id = master_user_id,
-            master_user_id = COALESCE(cte.master_agent_user_id, cte.publisher_user_id),
-            payed_by = cte.publisher_user_id
+            master_user_id = COALESCE(cte1.master_agent_user_id, cte2.publisher_user_id),
+            payed_by = cte2.publisher_user_id
         from
-            cte
+            cte1
+        inner join
+            cte2
+        on
+            cte1.offer_id = cte2.offer_id
         where
-            offers.offer_id = cte.offer_id
+            offers.offer_id = cte1.offer_id
     """
 
     await pg.get().execute(query, offer_ids)
--- tests/repositories/postgresql/test_update_offers_master_user_id_and_payed_by.py
+++ tests/repositories/postgresql/test_update_offers_master_user_id_and_payed_by.py
@@ -6,28 +6,39 @@ async def test_update_offers_master_user_id_and_payed_by():
     # arrange
     offer_ids = [1, 2, 3]
     query = """
-        with cte as (
+        with cte1 as (
         select
-            a.offer_id, b.master_agent_user_id, c.publisher_user_id
+            a.offer_id, b.master_agent_user_id
         from
             offers as a
         inner join
             agents_hierarchy as b on a.user_id = b.realty_user_id
-        inner join
-            offers_billing_contracts as c on a.offer_id = c.offer_id
-        where a.offer_id = ANY($1::BIGINT[])
+        where
+            a.offer_id = ANY($1::BIGINT[])
+        ), cte2 as (
+        select
+            c.offer_id, first_value (publisher_user_id)
+                        over (partition by offer_id order by row_version desc) as publisher_user_id
+        from
+            offers_billing_contracts as c
+        where
+            c.offer_id = ANY($1::BIGINT[])
         )
 
         update
             offers
         set
             old_master_user_id = master_user_id,
-            master_user_id = COALESCE(cte.master_agent_user_id, cte.publisher_user_id),
-            payed_by = cte.publisher_user_id
+            master_user_id = COALESCE(cte1.master_agent_user_id, cte2.publisher_user_id),
+            payed_by = cte2.publisher_user_id
         from
-            cte
+            cte1
+        inner join
+            cte2
+        on
+            cte1.offer_id = cte2.offer_id
         where
-            offers.offer_id = cte.offer_id
+            offers.offer_id = cte1.offer_id
     """
 
     # act
