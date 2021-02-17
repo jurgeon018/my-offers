f482a99204c0515a2e97d7faafe479c6c63bce3d
michail_shatalov <m.shatalov@cian.ru>
2021-02-17 18:58:42 +0200

    CD-100661: Правки тестов после ревью

--- tests/repositories/postgresql/test_get_offer_mobile_sorting.py
+++ tests/repositories/postgresql/test_get_offer_mobile_sorting.py
@@ -1,18 +1,21 @@
+import pytest
+
 from my_offers import enums
 from my_offers.repositories.postgresql.object_model import OFFER_TABLE, _prepare_sort_mobile_order
 
 
-async def test_delete_offers_duplicates(mocker):
+@pytest.mark.parametrize('sort_input', [
+    enums.MobOffersSortType.update_date,
+    enums.MobOffersSortType.move_to_archive_date,
+    enums.MobOffersSortType.price_asc,
+    enums.MobOffersSortType.price_desc,
+])
+async def test_prepare_sort_mobile_order(sort_input):
     # arrange
 
     # act
-    update_date = _prepare_sort_mobile_order(enums.MobOffersSortType.update_date)
-    move_to_archive_date = _prepare_sort_mobile_order(enums.MobOffersSortType.move_to_archive_date)
-    price_asc = _prepare_sort_mobile_order(enums.MobOffersSortType.move_to_archive_date)
-    price_desc = _prepare_sort_mobile_order(enums.MobOffersSortType.move_to_archive_date)
+    sort_result = _prepare_sort_mobile_order(sort_input)
 
     # assert
-    assert update_date[1] == OFFER_TABLE.offer_id
-    assert move_to_archive_date[1] == OFFER_TABLE.offer_id
-    assert price_asc[1] == OFFER_TABLE.offer_id
-    assert price_desc[1] == OFFER_TABLE.offer_id
+    assert len(sort_result) == 2
+    assert sort_result[1] == OFFER_TABLE.offer_id
