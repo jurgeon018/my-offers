1593f3a1eaffcd3bc0bc478cfc973d96de21954a
Denis Nikitin <d.nikitin@cian.ru>
2021-02-19 19:56:57 +0300

    fix

--- contrib/postgresql/migrations/0029-offers-row-versions-add-status-tab.sql
+++ contrib/postgresql/migrations/0029-offers-row-versions-add-status-tab.sql
@@ -1,3 +1,3 @@
 
 delete from offers_row_versions;
-alter table offers_row_versions add column status_tab offer_status_tab not null;
+alter table offers_row_versions add column status_tab offer_status_tab2 not null;
