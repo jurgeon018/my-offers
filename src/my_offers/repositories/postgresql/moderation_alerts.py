94c1572b804ecbacab8fafae19ebb9aa11365e37
michail_shatalov <m.shatalov@cian.ru>
2021-06-03 11:02:21 +0200

    CD-108974: Правки после ревью

--- src/my_offers/repositories/postgresql/moderation_alerts.py
+++ src/my_offers/repositories/postgresql/moderation_alerts.py
@@ -1,19 +1,18 @@
 from datetime import datetime
 
 import asyncpgsa
-import pytz
 from sqlalchemy.dialects.postgresql import insert
 
 from my_offers import pg
 from my_offers.repositories.postgresql import tables
 
 
-async def save_last_visit_date(user_id: int) -> None:
+async def save_last_visit_date(*, user_id: int, last_visit_date: datetime) -> None:
     insert_query = insert(tables.moderation_alerts)
 
     values = dict(
         user_id=user_id,
-        last_visit_date=datetime.now(tz=pytz.UTC),
+        last_visit_date=last_visit_date,
     )
 
     query, params = asyncpgsa.compile_query(
--- src/my_offers/services/moderation_alerts/_v1_hide_warnings.py
+++ src/my_offers/services/moderation_alerts/_v1_hide_warnings.py
@@ -1,11 +1,18 @@
+from datetime import datetime
+
+import pytz
+
 from my_offers import entities
 from my_offers.repositories.postgresql import save_last_visit_date
 
 
 async def v1_hide_warnings_public(
-        request: entities.MobileGetMyOffersRequest,
+        _: entities.MobileGetMyOffersRequest,
         realty_user_id: int
 ) -> None:
-    await save_last_visit_date(realty_user_id)
+    await save_last_visit_date(
+        user_id=realty_user_id,
+        last_visit_date=datetime.now(tz=pytz.UTC)
+    )
 
     return None
