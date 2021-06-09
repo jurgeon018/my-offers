19bf40687e74201fd8f8a886b68b2365547d496a
michail_shatalov <m.shatalov@cian.ru>
2021-06-09 13:10:14 +0200

    CD-109695: Добавление функционала last_visit_date в RS в запросе has-unread-warnings

--- src/my_offers/services/moderation_alerts/v1_has_unread_warnings.py
+++ src/my_offers/services/moderation_alerts/v1_has_unread_warnings.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from typing import Any, Dict, Optional
 
 import pytz
+from cian_core.runtime_settings import runtime_settings
 
 from my_offers import entities
 from my_offers.repositories.postgresql import get_declined_count_after_last_visit_date, get_last_visit_date
@@ -13,7 +14,11 @@ async def v1_has_unread_warnings_private(
 ) -> entities.HasUnreadWarningsResponse:
     filters: Dict[str, Any] = await get_user_filter(request.user_id)
 
-    last_visit_date: Optional[datetime] = await get_last_visit_date(user_id=request.user_id)
+    last_visit_date_from_db: Optional[datetime] = await get_last_visit_date(user_id=request.user_id)
+
+    last_visit_date: datetime = last_visit_date_from_db or datetime.utcfromtimestamp(
+        runtime_settings.get('DEFAULT_LAST_VISIT_DATE', 0)
+    ).replace(tzinfo=pytz.utc)
 
     declined_count: Optional[int] = await get_declined_count_after_last_visit_date(
         last_visit_date=last_visit_date or datetime.now(tz=pytz.UTC),
--- tests_functional/services/moderation_alerts/test_has_unread_warnings.py
+++ tests_functional/services/moderation_alerts/test_has_unread_warnings.py
@@ -117,7 +117,60 @@ async def test_v1_has_unread_warnings__no_moderation_warnings_since_last_visit__
     assert response.data == {'hasWarnings': False}
 
 
-async def test_v1_has_unread_warnings__has_moderation_warnings_without_last_visit__return_warnings(http, pg):
+async def test_v1_has_unread_warnings__has_moderation_warnings_from_set_rs_last_visit_date__return_warnings(
+        http,
+        pg,
+        runtime_settings,
+):
+    # arrange
+    user_id = 29437831
+    now = datetime.now(pytz.utc)
+
+    await pg.execute(
+        """
+        INSERT INTO public.offers (
+            offer_id,
+            master_user_id,
+            user_id,
+            deal_type,
+            offer_type,
+            status_tab,
+            services,
+            is_manual,
+            is_in_hidden_base,
+            has_photo,
+            search_text,
+            raw_data,
+            row_version,
+            created_at,
+            updated_at
+        )
+        VALUES
+            ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+        """,
+        [
+            123, user_id, user_id, 'sale', 'flat', 'declined', [], True, False, False, 'text',
+            '{}', 1, now + timedelta(hours=1), now + timedelta(hours=1),
+        ]
+    )
+
+    await runtime_settings.set({'DEFAULT_LAST_VISIT_DATE': 1623235883})
+
+    # act
+    response = await http.request(
+        'POST',
+        '/v1/has-unread-warnings/',
+        json={'user_id': user_id},
+    )
+
+    # assert
+    assert response.data == {'hasWarnings': True}
+
+
+async def test_v1_has_unread_warnings__has_moderation_warnings_from_default_rs_last_visit_date__return_warnings(
+        http,
+        pg,
+):
     # arrange
     user_id = 29437831
     now = datetime.now(pytz.utc)
