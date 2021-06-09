0138e3bc8d3c2f343eef1648ce52b71872fa4eff
michail_shatalov <m.shatalov@cian.ru>
2021-06-09 17:42:28 +0200

    CD-109695: Добавление функционала last_visit_date от 1970 года в запросе has-unread-warnings

--- src/my_offers/services/moderation_alerts/v1_has_unread_warnings.py
+++ src/my_offers/services/moderation_alerts/v1_has_unread_warnings.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from typing import Any, Dict, Optional
 
 import pytz
-from cian_core.runtime_settings import runtime_settings
 
 from my_offers import entities
 from my_offers.repositories.postgresql import get_declined_count_after_last_visit_date, get_last_visit_date
@@ -16,9 +15,7 @@ async def v1_has_unread_warnings_private(
 
     last_visit_date_from_db: Optional[datetime] = await get_last_visit_date(user_id=request.user_id)
 
-    last_visit_date: datetime = last_visit_date_from_db or datetime.utcfromtimestamp(
-        runtime_settings.get('DEFAULT_LAST_VISIT_DATE', 0)
-    ).replace(tzinfo=pytz.utc)
+    last_visit_date: datetime = last_visit_date_from_db or datetime.utcfromtimestamp(0).replace(tzinfo=pytz.utc)
 
     declined_count: Optional[int] = await get_declined_count_after_last_visit_date(
         last_visit_date=last_visit_date or datetime.now(tz=pytz.UTC),
