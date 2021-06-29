fc019bf377aa2b3d147804945b8fc1333abd4931
Vyacheslav Bezborodov <v.bezborodov@cian.ru>
2021-06-29 12:30:55 +0300

    CD-110959 add print temp

--- src/my_offers/services/agents/sync_agents.py
+++ src/my_offers/services/agents/sync_agents.py
@@ -7,6 +7,7 @@ import pytz
 from cian_core.runtime_settings import runtime_settings
 from cian_core.statsd import statsd
 from cian_http.exceptions import TimeoutException
+from my_offers.helpers.graphite import send_to_graphite
 
 from my_offers.repositories import postgresql
 from my_offers.repositories.agents import v1_get_agencies_with_activated_staff_service, v1_get_agents_list
@@ -26,7 +27,8 @@ async def _v1_get_agents_list(request: V1GetAgentsList) -> AgentsListResponse:
 
 async def sync_agents() -> None:
     response: GetAgenciesWithActivatedStaffServiceResponse = await v1_get_agencies_with_activated_staff_service()
-    for master_agent_user_id in response.user_ids:
+    for idx, master_agent_user_id in enumerate(response.user_ids):
+        print(f'sync_master_agent {idx} out of {len(response.user_ids)}')
         await _sync_master_agent(master_agent_user_id)
 
 
@@ -42,6 +44,7 @@ async def _paginate_sub_agents(master_agent_user_id: int) -> AsyncGenerator[Agen
         yield agent
 
     for page in range(2, init_paging_data.pages_count + 1):
+        print(f'page {page} out of {init_paging_data.pages_count}')
         response: AgentsListResponse = await _v1_get_agents_list(
             V1GetAgentsList(
                 user_id=master_agent_user_id,
@@ -82,6 +85,7 @@ async def _init_paging(master_agent_user_id: int) -> _InitPagingData:
 async def _sync_sub_agent(master_agent_user_id: int, sub_agent: AgentResponse) -> None:
     if not sub_agent.state.is_active:
         statsd.incr('sync-agents.skipped')
+        print('sync-agents.skipped', master_agent_user_id, sub_agent.user_id)
         return
     if await postgresql.set_agent_hierarchy_data(
         realty_user_id=sub_agent.user_id,
@@ -91,3 +95,4 @@ async def _sync_sub_agent(master_agent_user_id: int, sub_agent: AgentResponse) -
         updated_at=datetime.now(pytz.utc),
     ):
         statsd.incr('sync-agents.updated')
+        print('sync-agents.updated', master_agent_user_id, sub_agent.user_id)
