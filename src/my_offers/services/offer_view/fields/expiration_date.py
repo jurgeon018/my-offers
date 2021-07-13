20e647e5adf6b8dc9bb1d1306506427e656de107
Mikhail Sivykh <m.sivykh@cian.ru>
2021-07-13 16:31:31 +0300

    Do not send publish_till_date to the apps if auto-prolongation is turned on

--- src/my_offers/services/offer_view/fields/expiration_date.py
+++ src/my_offers/services/offer_view/fields/expiration_date.py
@@ -9,3 +9,13 @@ def get_expiration_date(publish_terms: Optional[PublishTerms]) -> Optional[datet
         return publish_terms.expiration_date
 
     return None
+
+
+def get_expiration_date_for_mobile(publish_terms: Optional[PublishTerms]) -> Optional[datetime]:
+    # не отправляем тонкому клиенту дату истечения срока, если включена автоматическая пролонгация т.к.
+    # дата не несёт полезной информации для пользователя, поскольку на деле не является датой окончания публикации,
+    # а лишь является датой следующей итерации списания средств
+    if publish_terms is None or publish_terms.autoprolong is True:
+        return None
+
+    return get_expiration_date(publish_terms=publish_terms)
