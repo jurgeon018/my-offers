91dcbe4e60ca51be7adefa7d95c3b88cc101b3c6
Vyacheslav Bezborodov <v.bezborodov@cian.ru>
2021-07-13 16:47:15 +0300

    CD-112623 handle bad request and remove Blocked

--- tests_functional/services/agents/test_change_agents_relations.py
+++ tests_functional/services/agents/test_change_agents_relations.py
@@ -13,8 +13,8 @@ async def _start(runner, queue_service, pg):
     await pg.execute_scripts(Path(__file__).parent / 'data' / 'change_agents_relations_setup.sql')
 
 
-@pytest.mark.parametrize('state', ['Request', 'Processing'])
-async def test_intermediate_states__remain_unchanged(queue_service, state, pg, agents_mock):
+@pytest.mark.parametrize('state', ['Request', 'Processing', 'Blocked'])
+async def test_uninteresting_states__remain_unchanged(queue_service, state, pg, agents_mock):
     # arrange
     await asyncio.gather(
         agents_mock.add_stub(
@@ -47,6 +47,60 @@ async def test_intermediate_states__remain_unchanged(queue_service, state, pg, a
     await queue_service.publish(
         'agents-relations-reporting.v1.changed',
         load_json_data(__file__, f'agents-relations-reporting.v1.changed--{state}.json'),
+        exchange='sub-agents',
+    )
+    await asyncio.sleep(0.5)
+    row = await pg.fetchrow('select master_user_id, user_id from offers where offer_id = $1', [173975523])
+
+    # assert
+    assert row == {'master_user_id': 3, 'user_id': 4}
+
+
+async def test_bad_request__remain_unchanged(queue_service, pg, agents_mock):
+    # arrange
+    await asyncio.gather(
+        agents_mock.add_stub(
+            mock_request.query['agentId'] == 1,
+            method='GET',
+            path='/v1/get-agent-info-by-id/',
+            response=MockResponse(
+                status=400,
+                body={
+                    "message": "Агент с ID 1 не найден",
+                    "errors": [
+                        {
+                            "message": "Агент с ID 1 не найден",
+                            "key": None,
+                            "code": None,
+                        }
+                    ]
+                },
+            ),
+        ),
+        agents_mock.add_stub(
+            mock_request.query['agentId'] == 2,
+            method='GET',
+            path='/v1/get-agent-info-by-id/',
+            response=MockResponse(
+                status=400,
+                body={
+                    "message": "Агент с ID 2 не найден",
+                    "errors": [
+                        {
+                            "message": "Агент с ID 2 не найден",
+                            "key": None,
+                            "code": None,
+                        }
+                    ]
+                },
+            ),
+        ),
+    )
+
+    # act
+    await queue_service.publish(
+        'agents-relations-reporting.v1.changed',
+        load_json_data(__file__, 'agents-relations-reporting.v1.changed--Active.json'),
         exchange='sub-agents',
     )
     await asyncio.sleep(0.5)
@@ -102,7 +156,7 @@ async def test_active_state__set_master_user_id(queue_service, state, pg, agents
     assert master_agent_user_id == 5
 
 
-@pytest.mark.parametrize('state', ['Blocked', 'Deleted', 'DeletedAndHidden'])
+@pytest.mark.parametrize('state', ['Deleted', 'DeletedAndHidden'])
 async def test_interesting_states__update_hierarchy(queue_service, state, pg, agents_mock):
     # arrange
     await asyncio.gather(
